b4e89b2d98fcdda380d3055ffae9106ca65c5bee
shatov130 <zoku130@outlook.jp>
2024-04-01 20:22:50 +0900

    ページ遷移せずに更新したい

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,8 +4,12 @@ class PokemonsController < ApplicationController
     @pokemon = Pokemon.find(params[:id])
     @pokemon.increment_victories
     redirect_to @pokemon
+    
   end
   def index
     @pokemons = Pokemon.all
+  end
+  def show
+    @pokemon = Pokemon.find(params[:id])
   end
 end
